42e750a3bcc301abbfef99564a9d113e3e28d53d
Guttikonda15 <115670769+Guttikonda15@users.noreply.github.com>
2022-11-18 23:46:28 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// getUsers button 
+ /*document.getElementById("btn-users").addEventListener('click', getUsers);
+
+function getUsers() {
+  fetch("http://localhost:3000/users/")
+  .then((res)=> res.json())
+ .then((data) => console.log(data))
+  .catch((err)=> console.log(err))
+ }*/
 const login = document.getElementById("login-page");
 const register = document.getElementById("register-Form");
 const note = document.getElementById("noteForm");
