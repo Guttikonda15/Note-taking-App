861a9ea8defa8f8274129322aae644272f063d12
Guttikonda15 <115670769+Guttikonda15@users.noreply.github.com>
2022-11-19 00:13:59 -0500

    Create user.js

---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+// "database" as object literal
+const users = [
+    {
+      UserName : "likhitha@gmail.com",
+      Password : "jhdbugubj"
+    },
+    {
+        UserName : "nikhitha@gmail.com",
+        Password : "fdgksejh"
+    },
+    {
+        UserName : "kumari@gmail.com",
+        Password : "ukjbgkh"
+    }
+  ];
+  
+  let getUsers = () => users;
+  module.exports = { getUsers };
